d008093c477d5734a8f6248df49b1e893fc0d5ee
ggw3vtg <qfreas@ups.com>
2018-12-19 15:45:18 -0500

    added Google API creds

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '../../../../node_modules/@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,9 @@ import { Router } from '../../../../node_modules/@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  loginFormModalEmail = new FormControl('', Validators.email);
+  loginFormModalPassword = new FormControl('', Validators.required);
 
   constructor(private router: Router) { }
 
